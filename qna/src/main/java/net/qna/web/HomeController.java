4651128a35ce1c67b51c44a953fbb5f6dd55c01b
KOJaeYong <mhanaro728@naver.com>
2015-09-11 19:59:17 +0900

    9_database settings & connection pooling

--- qna/src/main/java/net/qna/web/HomeController.java
+++ qna/src/main/java/net/qna/web/HomeController.java
@@ -3,11 +3,20 @@ package net.qna.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*spring mvc에서 해당 클래스를 컨트롤러로 인식하기 위해서 @Controller 라는 어노테이션 사용 */  
 @Controller
 public class HomeController {
+	/*Logger 추가*/
+	/*private static final Logger log = LoggerFactory.getLogger(HomeController.class);*/
+	
+	/*처음 사이트에 접속했을 때 http://localhost:8080/에 접근했을 때 이 메소드로 요청이 들어오도록 매핑
+	매핑하는 어노테이션 @RequestMapping("/") 매핑할 url은 /, home은 이동할 뷰의 이름 */	
 	@RequestMapping("/")
 	public String home() {
-		// /home.jsp
+		/*log.debug("logback setting success!");*/
+		// /home.jsp(prefix + view 이름 + suffix)
+		// prefix(/)은  webapp 폴더에 해당.
+		// webapp 폴더에 있는 home.jsp(물리적인 뷰 파일 결정) 파일을 요청 
 		return "home";
 	}
 }
--- qna/webapp/WEB-INF/jsp/home.jsp
+++ qna/webapp/WEB-INF/jsp/home.jsp
@@ -1,7 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 
-<%@ include file="./commons/_top.jspf"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>qna</title>
+<%@ include file="./commons/_head.jspf"%>
+</head>
+<body>
+	<%@ include file="./commons/_top.jspf" %>
 	<header class="jumbotron subhead" id="overview">
 	<div class="container">
 		<h1>DNdlti</h1>
